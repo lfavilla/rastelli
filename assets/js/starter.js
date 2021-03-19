fb1c1da51b2c5399a84ce5e1dacf54a24908f63c
lfavilla <luciano.favilla@gmail.com>
2021-03-19 10:24:11 +0100

    new files to ASSETS/JS folder

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -16,7 +16,7 @@
 // Import just what we need
 
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
-import "../../node_modules/popper.js/dist/popper.min.js";
+import "/assets/js/popper.min.js";
 
 import "../../node_modules/bootstrap/js/dist/util.js";
 import "../../node_modules/bootstrap/js/dist/modal.js";
