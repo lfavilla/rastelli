0ecda2f3b74949a0cbe4665d129cb299f89ec231
lfavilla <luciano.favilla@gmail.com>
2021-03-31 21:24:51 +0200

    H2 animation

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -74,6 +74,35 @@ $(function() {
   });
 });
 
+//Animation trigger
+(function() {
+  var elements;
+  var windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add('tracking-in-expand');
+        element.classList.remove('hidden');
+      }
+    }
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+})();
+
 // img-Lightbox
 imgLightbox("img-lightbox-link");
 
